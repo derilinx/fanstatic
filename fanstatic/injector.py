3761fc6a67922f997cefd7b3d445301012ac2a8d
Eric Soroos <eric-github@soroos.net>
2018-02-22 09:22:34 +0000

    Fix for utf/ascii decode issues

--- fanstatic/injector.py
+++ fanstatic/injector.py
@@ -62,7 +62,7 @@ class Injector(object):
 
         # The wrapped application may have `needed` resources.
         if needed.has_resources():
-            result = needed.render_topbottom_into_html(response.body)
+            result = needed.render_topbottom_into_html(response.unicode_body)
             # Reset the body...
             response.body = ''
             # And write the result. The `write` method handles unicode results.
